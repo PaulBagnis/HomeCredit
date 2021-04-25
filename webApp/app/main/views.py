6995a6ce167e813d96c443f1ff4960f58a9d18ac
Paul Bagnis <paul.bagnis@gmail.com>
2021-04-25 16:50:14 +0200

    Un plaisir de HTML et de JS
    C'est bien pour cette raison que j'ai choisi la spe logiciel, pour faire du front de merde toute ma vie et c'est un vrai plaisir

--- webApp/app/main/views.py
+++ webApp/app/main/views.py
@@ -10,6 +10,7 @@ from flask import (
     make_response)
 from app.main import main
 import pandas as pd
+import json
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
@@ -21,13 +22,36 @@ def predict():
         id = request.form['id']
 
     df = pd.read_csv("C:\\Users\\Paul_\\Desktop\\Projet Machine Learning\\Données\\application_train.csv")
-    chart1 = [['Loan Type', 'Yes or No']]
+    
+    user = df.loc[df['SK_ID_CURR'] == int(id)]
+    userLoan = user.to_dict()['NAME_CONTRACT_TYPE'][2]
+
+    chart1 = [['Loan Type', 'Paid', 'Didn\'t pay']]
     for row in df[1:].iterrows():
-        print(type(row[1]))
-        print(row[1])
-        #chart1.append([row['NAME_CONTRACT_TYPE'],row['TARGET']])
+        #print(type(row[1].to_dict()))
+        #print(row[1].to_dict())
+        tempDict = row[1].to_dict()
+        chart1.append([tempDict['TARGET'], tempDict['NAME_CONTRACT_TYPE']])
+
+    # For Chart1    
+    cash_r_0, cash_c_0, cash_c_1, cash_r_1 = 0, 0, 0, 0
+    for item in chart1[1:]:
+        if item[1] == "Cash loans" : 
+            if item[0] == 0 :
+                cash_c_0 += 1
+            else :
+                cash_c_1 += 1
+        elif item[1] == "Revolving loans" :
+            if item[0] == 0 :
+                cash_r_0 += 1
+            else :
+                cash_r_1 += 1
+    
+    mod_chart = json.dumps({ "array1" : [['Loan Type', 'Yes', 'No'], ['Cash Loans', cash_c_0, cash_c_1], ['Revolving loans', cash_r_0, cash_r_1]] })
+    #print(chart1)
+    
         
-    return render_template('predict.html', id=id, chart1=chart1)
+    return render_template('predict.html', id=id, mod_chart=mod_chart, userLoan=userLoan)
 
 # Page d'accueil avec form avec id
 # page avec predict et graphiques
